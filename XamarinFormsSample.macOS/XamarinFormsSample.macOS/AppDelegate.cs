7478046dbee3a291f2c65c643a6a272fbc4162f4
Aleksei Karasev <alex.v.karasev@gmail.com>
2021-09-17 16:54:11 +0300

    The sample project with the issue demo

---
+++ XamarinFormsSample.macOS/XamarinFormsSample.macOS/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace XamarinFormsSample.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+        public AppDelegate()
+        {
+            //var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var style = NSWindowStyle.Borderless;
+
+            var rect = new CoreGraphics.CGRect(0, 0, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Center();
+            window.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow => window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
